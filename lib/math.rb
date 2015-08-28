1eb5b7659bcff1332a7c2b31a1d8fe96d7b0fabf
BikeshDahal <bdahal2358@gmail.com>
2015-08-27 21:15:22 -0400

    Finished Lab

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,42 @@
 def addition(num1, num2)
+    
+    add=num1+num2;
+    add;
 end
 
 def subtraction(num1, num2)
+    
+    subt=num1-num2;
+    subt;
 end
 
 def division(num1, num2)
+    
+    div=num1/num2
+    div;
 end
 
 def multiplication(num1, num2)
+    
+    mult=num1*num2;
+    mult;
 end
 
 def modulo(num1, num2)
+    
+    modulo= num1%num2;
+    modulo;
 end
 
 def square_root(num)
+    
+    sq_rt= Math.sqrt(num);
+    sq_rt;
 end
 
 def order_of_operation(num1, num2, num3, num4)
+    
+    ord_ope= num1+((num2*num3)/num4);
+    ord_ope
   #Hint:  __ + (( __ * __ ) / __ )
 end
